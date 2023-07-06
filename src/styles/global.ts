4a7dfce9006ab1125cfce5ae241e83f08f32e1c3
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-07-06 14:51:44 -0300

    Eslint

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,11 +6,10 @@ export const globalStyles = globalCss({
     padding: 0,
     margin: 0,
   },
-  
+
   body: {
     background: '$gray900',
     color: '$gray100',
     '-webkit-font-smoothing': 'antialiased',
-    
-  }
+  },
 })
